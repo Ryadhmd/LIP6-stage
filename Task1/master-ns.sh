06fdeb81abd0945126a3b774b28b46bcec8b413c
Hamdini Cherif <ryadhamdini@gmail.com>
2023-06-22 13:07:08 +0200

    fixing master empty

--- Task1/master-ns.sh
+++ Task1/master-ns.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
 
-unshare -p -u -n --fork --mount-proc sleep infinity 1 &
-master=pgrep -fn "sleep infinity 1" 
+unshare -p -u -n --fork --mount-proc sleep infinity 1 && master=$(pgrep -fn "sleep infinity 1") 
 nsenter -u -t $master hostname master
 
+ip link add veth0 type veth peer name veth1
+ip link set veth1 netns $master 
+
+
 nsenter -n -u -p -m -t $master bash 
+ 
+ 
+
+ 
  
