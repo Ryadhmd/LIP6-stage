249a825bacb548a59866bab3eb5b0a447bf64eb1
Hamdini Cherif <ryadhamdini@gmail.com>
2023-06-27 16:16:18 +0200

    ns detailled

--- Task1/master-ns.sh
+++ Task1/master-ns.sh
@@ -5,11 +5,15 @@ sleep 1
 master=$(pgrep -fn "sleep infinity 1") 
 nsenter -u -t $master hostname master
 
-ip link add veth0 type veth peer name veth1
-ip link set veth1 netns $master 
 
+ip link add veth0 type veth peer name veth1
+ip link set up veth0 
+ip addr add 192.168.1.254/24 dev veth0
+
+ip link set veth1 netns $master 
 nsenter -n -u -p -m -t $master bash 
- 
+ip link set up lo
 ip link set up veth1
+
 ip addr add 192.168.1.2/24 dev veth1
 ip route add default via 192.168.1.254 
