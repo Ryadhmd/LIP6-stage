23e50e62daf049f0efbb5aa8efdbc585957a9c4d
Hamdini Cherif <ryadhamdini@gmail.com>
2023-06-30 10:55:48 +0200

    loading modules

--- Task1/check-modules.sh
+++ Task1/check-modules.sh
@@ -1,7 +1,15 @@
+
 #!/bin/bash
 
+# Check if the file path argument is provided
 if [[ -z "$1" ]]; then
   echo "Usage: $0 <module_file>"
+  exit 1
+fi
+
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
   exit 1
 fi
 
@@ -19,9 +27,20 @@ readarray -t modules < "$module_file"
 # Loop through each module and check if it is loaded
 for module in "${modules[@]}"; do
   if ! lsmod | grep -wq "${module}"; then
-    echo "Module '${module}' is not loaded."
-  fi
+	echo "Module '${module}' is not loaded."
+	
+	if ! modinfo $module &> /dev/null; then
+    		echo "Module $module is not installed."
+	else
+    		echo "Module $module is installed."
+    	if modprobe $module; then
+       		echo "Module $module loaded successfully."
+    	else
+      	 	echo "Failed to load module $module."
+    	fi   	 
+    	echo "-----------------------------"
+	fi
+  fi  
 done
-
 
 
